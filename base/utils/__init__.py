8874f3982a1ffba5be6cd9f49e96885436382b83
8uos <user@AL01329043.local>
2021-12-22 12:52:48 +0900

    rm NSMLwriter

--- base/utils/__init__.py
+++ base/utils/__init__.py
@@ -8,10 +8,10 @@ from .utils import (
     add_dim_and_reshape, AverageMeter, AverageMeters, accuracy, temporary_freeze, freeze, unfreeze, rm
 )
 from .visualize import refine, make_comparable_grid, save_tensor_to_image
-from .writer import DiskWriter, TBDiskWriter, NSMLWriter
+from .writer import DiskWriter, TBDiskWriter
 from .load import load_reference, load_primals, load_decomposition
 from .config import setup_train_config
 
 
 __all__ = [
-    "Logger", "add_dim_and_reshape", "AverageMeter", "AverageMeters", "accuracy", "temporary_freeze", "freeze", "unfreeze", "rm", "refine", "make_comparable_grid", "save_tensor_to_image", "DiskWriter", "TBDiskWriter", "NSMLWriter", "load_reference", "load_primals", "load_decomposition", "setup_train_config"]
+    "Logger", "add_dim_and_reshape", "AverageMeter", "AverageMeters", "accuracy", "temporary_freeze", "freeze", "unfreeze", "rm", "refine", "make_comparable_grid", "save_tensor_to_image", "DiskWriter", "TBDiskWriter", "load_reference", "load_primals", "load_decomposition", "setup_train_config"]
